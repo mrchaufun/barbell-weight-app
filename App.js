8bcb11e417573d9a1f1932edbc40b2b4191f5529
mrchaufun <justinchau31@gmail.com>
2020-02-05 10:34:44 -0800

    Home Screen Created

--- App.js
+++ App.js
@@ -1,19 +1,38 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Container, Text, Button, Content, Body } from 'native-base';
+import { Stylesheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import * as Font from 'expo-font';
 
-export default function App() {
+const HomeScreen = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <Container>
+      <Content>
+        <Body>
+          <Text style={{ marginTop: 10, fontSize: 20 }}>Which bar are you using?</Text>
+          <Button primary rounded style={{ marginVertical: 120 }}>
+           <Text>Mens Bar</Text>
+          </Button>
+          <Button danger rounded>
+            <Text>Womens Bar</Text>
+          </Button>
+        </Body>
+      </Content>
+    </Container>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
